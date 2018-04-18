7b882ad9658e83012d4fbb7f5c520c9a9f4bb4f9
cuiran <cranmail@gmail.com>
2018-04-18 17:22:43 +0000

    Partially defined the data class. Wrote place holder functions.

--- data_processing.py
+++ data_processing.py
@@ -3,7 +3,20 @@ import pandas as pd
 
 class data:
     def __init__(self,args):
-        data.ld = args.ld
+        self.ld = args.ld
+        self.ss = args.ss
+        self.weights = args.weights
+        self.annot_SNPs = args.annot_snplist
+        self.test_region = args.leave_out
+        self.train_ind = []
+        self.test_ind = []
+    
+    def get_traintest_indices(self,args.leave_out):
+        # get train and test indices
+        
 
 def process(args):
     return data(args)
+
+def match_SNPs(args):
+    return
