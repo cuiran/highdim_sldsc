669e828a20cb8a27d1133f495b0f438a247e91c2
cuiran <cranmail@gmail.com>
2018-04-18 23:50:16 +0000

    Added flag --chrsnp-list, this flag is for user to input a file name that contains chromosomes and snps. This file will mainly be used to split data into training and test.

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ if __name__=='__main__':
     parser.add_argument('--ld',help='The .h5 file combining ldscores for all annotations')
     parser.add_argument('--sumstats',help='Text file of formatted summary statistics with columns SNP, A1, A2, N, CHISQ, Z, where CHISQ is Z^2')
     parser.add_argument('--annot-snplist',help='The text file listing all SNPs used to create annotations. This file should contain two columns, one is CHR, the other is SNP')
+    parser.add_argument('--chrsnp-list',help='Provide a file with two columns CHR and SNP, the order matches the ones in annot ld SNPs.')
     parser.add_argument('--leave-out',help='Specify the region to leave out, for example, chr22')
     parser.add_argument('--reg-method',help='Name of the method to run regression with. Choose among OLS,Lasso,Lasso+OLS,elnet,grpLasso,skLassoCV,skOLS')
     parser.add_argument('--output-folder',help='Point to a location where the program can store output files')
