7fc29a292a5edd867a39b1b0ed4baa8e9644b1c7
cuiran <cranmail@gmail.com>
2018-04-24 01:54:08 +0000

    Started to define the parent object "regression".

--- regression.py
+++ regression.py
@@ -1,8 +1,13 @@
 import numpy as np
 
-class reg:
-    def __init__(self,args,data):
-        reg.coef = "place holder"
+class regression:
+    def __init__(self,fit_intercept=True,normalize=True,lr=0.01,decay=0.,momentum=0.,alpha=1.0):
+        self.alpha = alpha
+        self.fit_intercept = fit_intercept
+        self.normalize = normalize
+        self.lr = lr
+        self.decay = decay
+        self.momentum = momentum
 
-def perform_regression(args,data):
-    return reg
+
+class Lasso(regression):
