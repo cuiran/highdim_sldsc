c52d0cc5702de75760cca7c50472d2e5945b7858
cuiran <cranmail@gmail.com>
2018-04-25 18:56:44 +0000

    Compute true weights and scale it to have variance 1 at the very beginning. Change flag --weights into --weights_ld to avoid confusion. Weights_ld is the user specified regression SNP ld scores.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import pandas as pd
 import argparse
 import data_processing as d
 import reg_main as r
 import post_processing as p
 import pdb
+
+
 def highdim_sldsc(args):
     """ This function 
         1. process user input
@@ -16,13 +19,26 @@ def highdim_sldsc(args):
         Save the un-standardized coefficients and bias to file
     """
     d.match_SNPs(args)
+    num_SNPs = d.get_num_SNPs(args)
+    original_data = d.data(args.ld,args.sumstats,args.weights_ld,range(num_SNPs))
     train_ind,test_ind = d.get_traintest_ind(args)
-    train_data = d.data(args.ld,args.sumstats,args.weights,train_ind)
-    #change chisq to chisq-1, compute true weights on taining data
-    ready_data = d.process(args,train_data)
+    # from weights_ld, annotation ld, and sumstats compute true weights
+    # scale weights to have variance 1
+    weights = d.compute_true_w(args,original_data)
+    weights_df = pd.DataFrame(data=weights,columns=['TRUE_W'])
+    weights_fname = args.output_folder+'true_weights.txt'
+    weights_df.to_csv(weights_fname,sep='\t',index=False)
+    weights_scaled = d.scale_weights(weights) #TODO: write this function
+    s_weights_df = pd.DataFrame(data=weights_scaled,columns=['SCALED_W'])
+    weights_fname = args.output_folder+'scaled_weights.txt'
+    s_weights_df.to_csv(weights_fname,sep='\t',index=False)
+    #TODO: put the above weights processing into one function in data_processing.py
+    # form train and test data objects
+    train_data = d.data(args.ld,args.sumstats,weights_fname,train_ind)
+    test_data = d.data(args.ld,args.sumstats,weights_fname,test_ind)
     #perform regression with specified parameters on ready_data
-    reg = r.perform_regression(args,ready_data)
-    p.process(args,reg,ready_data)
+    reg = r.perform_regression(args,train_data)
+    p.process(args,reg,test_data)
     return
 
 
@@ -32,7 +48,7 @@ if __name__=='__main__':
     parser.add_argument('--highdim-sldsc',action='store_true',help='run high dimensional stratified ldscore regression')
     parser.add_argument('--ld',help='The .h5 file combining ldscores for all annotations')
     parser.add_argument('--sumstats',help='Text file of formatted summary statistics with columns SNP, A1, A2, N, CHISQ, Z, where CHISQ is Z^2')
-    parser.add_argument('--weights',help='The user input weights. Chrom separated "weights.#chr.l2.ldscore.gz" files input as --weights weights.')
+    parser.add_argument('--weights_ld',help='The user input weights. Chrom separated "weights.#chr.l2.ldscore.gz" files input as --weights weights.')
     parser.add_argument('--annot-snplist',help='The text file listing all SNPs used to create annotations. This file should contain two columns CHR and SNP. The oder matches the oder of SNPs used in --ld.')
     parser.add_argument('--leave-out',help='Specify the region to leave out, for example, chr22')
     parser.add_argument('--reg-method',help='Name of the method to run regression with. Choose among OLS,Lasso,Lasso+OLS,elnet,grpLasso,skLassoCV,skOLS')
