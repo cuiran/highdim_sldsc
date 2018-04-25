5b9460c146ffc388551e352a8338851138d2fc96
cuiran <cranmail@gmail.com>
2018-04-25 18:58:36 +0000

    Work flow for regression is now: define regression object, fit regression, return reg_obj with learned coef and intercept.

--- reg_main.py
+++ reg_main.py
@@ -1,4 +1,17 @@
 import regression as regr
+import pdb
 
 def perform_regression(args,data):
-    return regression()
+    if args.reg_method == 'OLS':
+        regr.OLS()
+    elif args.reg_method == 'Lasso':
+        lasso = regr.Lasso()
+        lasso.fit(data)
+        reg_obj = lasso
+    elif args.reg_method == 'ElasticNet':
+        regr.ElasticNet()
+    elif args.reg_method == 'LassoOLS':
+        regr.LassoOLS()
+    else:
+        raise ValueError('The value of --reg-method is invalid')
+    return reg_obj
