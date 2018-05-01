62017ad46a83c324b6a01bc277c320204881879f
cuiran <cranmail@gmail.com>
2018-05-01 16:19:57 +0000

    Changed name of function to compute_final_w to better reflect on what's done. Moved some small utility funtions to useful_functions.py. Wrote weights_processing function.

--- data_processing.py
+++ data_processing.py
@@ -45,9 +45,9 @@ def get_traintest_ind(args):
         print('--leave-out functionality is not complete.')
     return train_ind,test_ind
 
-def compute_true_w(args,data):
+def compute_final_w(args,data):
     # concatenate weights in args.weights_ld
-    # this is the part of true weights that correct some of the correlated errors
+    # this is the part of final weights that correct some of the correlated errors
     chr_list = [str(i) for i in range(1,23)]
     weights_fnames = [args.weights_ld+x+'.l2.ldscore.gz' for x in chr_list]
     weights_dfs = [pd.read_csv(x,delim_whitespace=True) for x in weights_fnames]
@@ -57,35 +57,19 @@ def compute_true_w(args,data):
     # compute weights that correct some of the heteroskedasticity
     M = len(data.active_ind)
     M = float(M)
-    sum_trainld = h5_sum_all(data.X,data.active_ind)
-    sum_trainss = chisq_sum_all(data.y,data.active_ind)
+    sum_trainld = u.h5_sum_all(data.X,data.active_ind)
+    sum_trainss = u.chisq_sum_all(data.y,data.active_ind)
     l = sum_trainld/M
     s = sum_trainss/M
     Ntau_hat = np.divide(s-1,l)
-    weights_hetero = 2*((Ntau_hat*h5_sum_cols(data.X,data.active_ind)+1)**2)
+    weights_hetero = 2*((Ntau_hat*u.h5_sum_cols(data.X,data.active_ind)+1)**2)
     # multiply heteroskedasticity weights with correlation weights
-    true_weights = np.multiply(weights_hetero,weights_corr)
-    return true_weights
+    final_weights = np.multiply(weights_hetero,weights_corr)
+    return final_weights
 
-def h5_sum_all(h5_file, active_ind):
-    # sum all elements in the h5 file. The file must have key 'dataset'
-    f = h5py.File(h5_file,'r')
-    d = f['dataset']
-    s = np.sum(d[active_ind,:])
-    return s
-
-def h5_sum_cols(h5_file,active_ind):
-    # sum columns in the h5 file. The file must have key 'dataset'
-    f = h5py.File(h5_file,'r')
-    d = f['dataset']
-    s = np.sum(d[active_ind,:],axis=1)
-    return s
-
-def chisq_sum_all(ss_file, active_ind):
-    ss_df = pd.read_csv(ss_file,delim_whitespace=True)
-    chisq = np.array(ss_df['CHISQ'])
-    return np.sum(chisq)
-
-def get_num_SNPs(args):
-    snplist_df = pd.read_csv(args.annot_snplist,delim_whitespace=True)
-    return snplist_df.shape[0]
+def weights_processing(args,original_data):
+    final_weights = compute_final_w(args,original_data)
+    df = pd.DataFrame(data=final_weights,columns=['TRUE_W'])
+    weights_fname = args.output_folder+'final_weights.txt'
+    df.to_csv(weights_fname,sep='\t',index=False)
+    return
