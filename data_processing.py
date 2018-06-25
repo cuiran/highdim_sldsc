Fixed the weighting scaling process of fitting small data with sklearn.

data_processing.py
@@ -66,30 +66,44 @@ class data:
                 length += i[1]-i[0]
             self._active_len = length
         return self._active_len
-
+#TODO improve algorithm
     @property
     def mean_X(self):
         if not self._mean_X:
-            d = u.read_h5(data.X)
-            sum_active_rows = 0
-            for i in self.active_ind:
-                start,end = i
-                sum_chunck = np.sum(d[start:end,:],axis=0)
-                sum_active_rows += sum_chunck
-            self._mean_X = np.divide(sum_active_rows,self.active_len)
+            X = u.read_h5(self.X)
+            mean = []
+            std = []
+            for strip in self.X_strips:
+                X_strip = u.get_strip_active_X(X,strip,self.active_ind)
+                mean_strip = np.mean(X_strip,axis=0)
+                mean.append(mean_strip)
+                std_strip = np.std(X_strip,axis=0)
+                std.append(std_strip)
+            mean = np.array(mean)
+            std = np.array(std)
+            self._mean_X = mean.flatten()
+            self._std_X = std.flatten()
         return self._mean_X
 
     @property
     def std_X(self):
         if not self._std_X:
-            d = u.read_h5(data.X)
-            sum_sqdiff = 0
-            for i in self.active_ind:
-                start,end = i
-                sum_sqdiff_chunck = np.sum((d[start:end,:] - self.mean_X)**2,axis=0)
-                sum_sqdiff += sum_sqdiff_chunck
-            self._std_X = np.sqrt(np.divide(sum_sqdiff,self.active_len))
+            self.mean_X(self)
         return self._std_X
+
+    @property
+    def mean_y(self):
+        if not self._mean_y:
+            y = u.read_chisq_from_ss(self.y,self.active_ind)
+            self._mean_y = np.mean(y)
+            self._std_y = np.std(y)
+        return self._mean_y
+
+    @property
+    def std_y(self):
+        if not self._std_y:
+            self.mean_y(self)
+        return self._std_y
  
     @property
     def sum_active_weights(self):

regression.py
@@ -30,7 +30,7 @@ class regression:
         #TODO there's something wrong with this function
         d = u.read_h5(data.X)
         val_annotld = u.get_active(d,data.active_ind) # output one ndarray represent validation matrix
-        val_chisq = u.read_chisq_from_ss(data.y,data.active_ind).reshape(-1,1) # ndarray of validation chisq stats
+        val_chisq = u.read_chisq_from_ss(data.y,data.active_ind)# ndarray of validation chisq stats
         val_weights = u.get_active_weights(data.weights,data.active_ind) # ndarray of validation weights
         if val_annotld.ndim == 1:
             val_annotld = val_annotld[np.newaxis].T
@@ -86,16 +86,12 @@ class sk_LassoCV(regression):
         super().__init__(**kwargs)
 
     def fit(self,data):
-        X,y = u.preprocess_data(data)
-        w = np.sqrt(u.get_active_weights(data.weights,data.active_ind))
-        w = np.array(w)
-        wX,wy = u.weight_Xy(w,X,y)
+        new_X,new_y,X_scale,y_scale = u.preprocess_data(data)
         from sklearn.linear_model import LassoCV
         model = LassoCV(fit_intercept=False)
-        model.fit(wX,wy)
+        model.fit(new_X,new_y)
         learned_coef = model.coef_
-        true_coef = (learned_coef/data.X_scale)/data.N
-        true_intercept = data.weighted_meany - data.weighted_meanX.dot(learned_coef/data.X_scale)
+        true_coef,true_intercept = recover(data,learned_coef,X_scale,y_scale)
         self.coef = true_coef
         self.intercept = true_intercept
         self.alpha = model.alpha_
@@ -106,15 +102,12 @@ class sk_OLS(regression):
         super().__init__(**kwargs)
 
     def fit(self,data):
-        X,y = u.preprocess_data(data)
-        w = np.sqrt(u.get_active_weights(data.weights,data.active_ind))
-        wX,wy = u.weight_Xy(w,X,y) 
+        new_X,new_y,X_scale,y_scale = u.preprocess_data(data)
         from sklearn.linear_model import LinearRegression
         model = LinearRegression(fit_intercept=False)
-        model.fit(wX,wy)
+        model.fit(new_X,new_y)
         learned_coef = model.coef_
-        true_coef = (learned_coef/data.X_scale)/data.N
-        true_intercept = data.weighted_meany - data.weighted_meanX.dot(learned_coef/data.X_scale)
+        true_coef,true_intercept = recover(data,learned_coef,X_scale,y_scale)
         self.coef = true_coef
         self.intercept = true_intercept
         return self.coef,self.intercept
@@ -133,6 +126,12 @@ class sk_OLS(regression):
         self.coef = model.coef_/data.N
         self.intercept = model.intercept_
         return self.coef, self.intercept
+
+def recover(data,learned_coef,X_scale,y_scale):
+    Ntrue_coef = learned_coef*y_scale/X_scale
+    true_coef = Ntrue_coef/data.N
+    true_intercept = data.mean_y - data.mean_X.dot(Ntrue_coef)
+    return true_coef,true_intercept
 
 def recover_coef_intercept(data,learned_coef):
     """ Recover true coef and intercept from the learned coef

useful_functions.py
@@ -189,13 +189,33 @@ def preprocess_data(data):
     # return the centered scaled active X and centered active y
     active_X = get_active(read_h5(data.X),data.active_ind)
     active_y = read_chisq_from_ss(data.y,data.active_ind)
-    new_X,new_y = center_scale_Xy(active_X,active_y,data)
-    return new_X,new_y
+    centered_X,centered_y = center(active_X,active_y,data)
+    w = get_active_weights(data.weights,data.active_ind)
+    sqrt_w = np.sqrt(w)
+    wX,wy = weight_Xy(w,centered_X,centered_y)
+    new_X,new_y,X_scale,y_scale = scale(wX,wy)
+    return new_X,new_y,X_scale,y_scale
 
 def weight_Xy(w,X,y):
     # this is for small data
     # multiply X and y by w
     # w is approximately the square root of estimated variance
-    new_X = w.reshape(-1,1)*X
+    if X.ndim == 1:
+        X = X[:,None]
+    new_X = w[:,None]*X
     new_y = w*y
     return new_X,new_y
+
+def center(X,y,data):
+    centered_X = X - data.mean_X
+    centered_y = y - data.mean_y
+    return centered_X,centered_y
+
+def scale(X,y):
+    # this is for small data
+    # return X/std(X), y/std(y)
+    X_scale = np.std(X,axis=0)
+    y_scale = np.std(y,axis=0)
+    return X*(1/X_scale),y*(1/y_scale),X_scale,y_scale
+
+