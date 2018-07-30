efe5d6c738df0447c60ff811bc6c9e064a9a9b56
cuiran <cranmail@gmail.com>
2018-07-30 15:40:08 +0000

    tested data process with simulation

--- test_regression/test_dataprocess.py
+++ test_regression/test_dataprocess.py
@@ -16,24 +16,17 @@ def gen_data():
     beta_df = pd.DataFrame(data=beta,columns=['BETA'])
     beta_df.to_csv('beta.txt',sep='\t',index=False)
     X = np.random.rand(N,p)
-    X = preprocessing.scale(X)
+    f = h5py.File('X.h5','w')
+    f.create_dataset('dataset',data=X)
+    f.close()
     noise_cov = np.identity(N)
     noise = np.random.multivariate_normal(np.zeros(N),noise_cov)
     y = X.dot(beta) + noise
     data_obj = save_make_obj(X,y,noise_cov)
     return data_obj,X,y
 
-def fit():
-    data_obj,X,y = gen_data()
-    sklasso = linear_model.LassoCV()
-    sklasso.fit(X,y)
-    klasso = r.Lasso()
-    klasso.fit(data_obj,data_obj)
-    return klasso,sklasso,data_obj
-
-
 def save_make_obj(X,y,noise_cov):
-    dd = d.data('X_processed.h5','y_processed.txt','w_processed.txt',[[0,len(y)]])
+    dd = d.data('X.h5','y.txt','w.txt',[[0,len(y)]])
     f = h5py.File(dd.X,'w')
     f.create_dataset('dataset',data=X)
     f.close()
@@ -42,22 +35,21 @@ def save_make_obj(X,y,noise_cov):
     weights = np.diagonal(noise_cov)
     w_df = pd.DataFrame(data=weights,columns=['WEIGHT'])
     w_df.to_csv(dd.weights,sep='\t',index=False)
-    dd._active_len = len(y)
     dd._N = 1.0
-    dd._mean_X = np.mean(X,axis=0)
-    dd._std_X = np.std(X,axis=0)
-    dd._mean_y = np.mean(y)
-    dd._std_y = np.std(y)
-    dd._num_features = X.shape[1]
-    dd.X_offset = dd._mean_X
-    dd.X_scale = dd._std_X
-    dd.y_scale = dd._std_y
-    dd.y_offset = dd._mean_y
-    y = (y - dd.y_offset)/dd.y_scale
-    y_df = pd.DataFrame(data=y,columns=['CHISQ'])
-    y_df.to_csv(dd.y,sep='\t',index=False)
     return dd
-        
+ 
+def fit():
+    data_obj,X,y = gen_data()
+    sklasso = linear_model.LassoCV()
+    sklasso.fit(X,y)
+    klasso = r.Lasso(alpha=sklasso.alpha_)
+    processed_data = d.preprocess_large(data_obj)
+    klasso.fit(processed_data,data_obj)
+    return klasso,sklasso,data_obj
+
+
+
+       
 
 if __name__ == '__main__':
     num_simulations = 5
@@ -73,7 +65,7 @@ if __name__ == '__main__':
         sklasso_coef.append(sklasso.coef_.flatten())
         klasso_alpha.append(klasso.alpha)
         sklasso_alpha.append(sklasso.alpha_)
-        original_data = d.data('X_processed.h5','y.txt','w_processed.txt',[[0,1000]])
+        original_data = d.data('X.h5','y.txt','w.txt',[[0,1000]])
         original_data._N = 1.0
         klasso.evaluate_weighted(original_data)
         klasso_wsse.append(klasso.wsse)
@@ -81,9 +73,9 @@ if __name__ == '__main__':
     df['klasso_alpha'] = klasso_alpha
     df['sklasso_alpha'] = sklasso_alpha
     df['klasso_wsse'] = klasso_wsse
-    df.to_csv('diffalpha_test_alpha_wsse.csv',sep='\t',index=False)
+    df.to_csv('testprocess_samealpha_alpha_wsse.csv',sep='\t',index=False)
     coef_df = pd.DataFrame(data=klasso_coef,columns=[x for x in range(1,len(klasso_coef[0])+1)])
-    coef_df.to_csv('diffalpha_test_klassocoef_sim.csv',sep='\t',index=False)
+    coef_df.to_csv('testprocess_samealpha_klassocoef_sim.csv',sep='\t',index=False)
     coef_df = pd.DataFrame(data=sklasso_coef,columns=[x for x in range(1,len(sklasso_coef[0])+1)])
-    coef_df.to_csv('diffalpha_test_sklassocoef_sim.csv',sep='\t',index=False)
+    coef_df.to_csv('testprocess_samealpha_sklassocoef_sim.csv',sep='\t',index=False)
     
